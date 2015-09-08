ad3a9611f3661ed1ef13cb13cb5a4e0193496857
sheryl <sherylhsu@agtopnet.com>
2015-09-09 02:17:37 +0800

    [Title] Fix comflict [Symptom] [Root Cause] [Solution]

--- venv/sitecopy.py
+++ venv/sitecopy.py
@@ -6,6 +6,21 @@ import paramiko
 import subprocess as sub
 import threading
 from HTMLParser import HTMLParser
+from Queue import Queue
+import datetime
+
+#multi-thread
+class Job:
+     def __init__(self, threadname, server, username, password, sourcefile):
+             self.name = threadname
+             self.server =  server
+             self.username = username
+             self.password = password
+             self.sourcefile = sourcefile
+     def do(self):
+             #time.sleep(2)
+
+             print("\t[Info] Job({0}) is done!".format(self.name))
 
 class RunCmd(threading.Thread):
     def __init__(self, cmd, timeout):
@@ -230,107 +245,116 @@ def RemoveStringSpace(inputSTR):
 #def check_ssh(ip, user, key_file, initial_wait=0, interval=0, retries=1):
 
 #Download partial from sohu TV and combine
-def DownloadVideo(inputURL, IP, Port, ServerIP, User, PassWord):
-  #(http_proxy=http://218.60.56.95:8080 ./youtube-dl -g --get-filename http://tv.sohu.com/20140923/n404564864.shtml)
-  print 'Start of getting URL'
-  partial_element = inputURL.split('tv.sohu.com/')
-  partial_element[1]=partial_element[1].replace('/', '_')
-  cmd_to_execute = '(http_proxy=http://'+IP+':'+Port+' ./youtube-dl -g --get-filename '+inputURL+') > source/'+partial_element[1]+'.txt'
-  os.system(cmd_to_execute)
-  print 'End of getting URL'
-  with open('source/'+partial_element[1]+'.txt') as current_file:
-    partial_array = current_file.readlines()
-  GetVideoName = GetVideoFilename()
-  GetVideoName.GetVideoTitle(inputURL)
-  cmd_to_execute = 'mkdir Download/'+GetVideoName.filename
-  os.system(cmd_to_execute)
-  if len(partial_array) == 0:
-    return True
-  for i in range(0, (len(partial_array)-1), 1):
-    if i%2 == 1:
-      partial_element = partial_array[i].split('-')
-      partial_name = GetVideoName.filename+partial_element[1]
-      partial_name = partial_name.replace('\n', '')
-      print partial_name+'/'+partial_url
-      cmd_to_execute = 'wget -O Download/'+GetVideoName.filename+'/'+partial_name+' '+partial_url+' --proxy=on -c -e "http_proxy=http://'+IP+':'+Port+'"'
-      os.system(cmd_to_execute)
-    else:
-      partial_url = partial_array[i].replace('\n', '')
-  #Combine video
-  #require: sudo apt-get install mencoder
-  #mencoder -ovc copy -oac mp3lame -idx -o out.mp4 *.mp4
-  partial_element = partial_name.split('.')
-  cmd_to_execute = 'mencoder -ovc copy -oac mp3lame -idx -o Download/Finish/'+GetVideoName.filename+'.'+partial_element[(len(partial_element)-1)]+' Download/'+GetVideoName.filename+'/'+GetVideoName.filename+'*.'+partial_element[(len(partial_element)-1)]
-  os.system(cmd_to_execute)
-  return False
+class DownloadVideoViaSSH():
+  def __init__(self, ServerIP, User, PassWord):
+    self.ssh = paramiko.SSHClient()
+    print ServerIP+','+User+','+PassWord
+    self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    #sleep(initial_wait)
+    for i in range(2):
+      try:
+        self.ssh.connect(ServerIP, username=User, password=PassWord)
+        break
+      except (BadHostKeyException, AuthenticationException,
+              SSHException, socket.error) as e:
+        print e
+        #sleep(interval)
+  def DownloadVideo_local(self, inputURL, IP, Port, ServerIP, User, PassWord):
+    #(http_proxy=http://218.60.56.95:8080 ./youtube-dl -g --get-filename http://tv.sohu.com/20140923/n404564864.shtml)
+    print 'Start of getting URL'
+    partial_element = inputURL.split('tv.sohu.com/')
+    partial_element[1]=partial_element[1].replace('/', '_')
+    cmd_to_execute = '(http_proxy=http://'+IP+':'+Port+' ./youtube-dl -g --get-filename '+inputURL+') > source/'+partial_element[1]+'.txt'
+    os.system(cmd_to_execute)
+    print 'End of getting URL'
+    with open('source/'+partial_element[1]+'.txt') as current_file:
+      partial_array = current_file.readlines()
+    GetVideoName = GetVideoFilename()
+    GetVideoName.GetVideoTitle(inputURL)
+    cmd_to_execute = 'mkdir Download/'+GetVideoName.filename
+    os.system(cmd_to_execute)
+    if len(partial_array) == 0:
+      return True
+    for i in range(0, (len(partial_array)-1), 1):
+      if i%2 == 1:
+        partial_element = partial_array[i].split('-')
+        partial_name = GetVideoName.filename+partial_element[1]
+        partial_name = partial_name.replace('\n', '')
+        print partial_name+'/'+partial_url
+        cmd_to_execute = 'nohup wget -O Download/'+GetVideoName.filename+'/'+partial_name+' '+partial_url+' --proxy=on -c -e "http_proxy=http://'+IP+':'+Port+'" >/dev/null &'
+        os.system(cmd_to_execute)
+      else:
+        partial_url = partial_array[i].replace('\n', '')
+    #Combine video
+    #require: sudo apt-get install mencoder
+    #mencoder -ovc copy -oac mp3lame -idx -o out.mp4 *.mp4
+    partial_element = partial_name.split('.')
+    cmd_to_execute = 'nohup mencoder -ovc copy -oac mp3lame -idx -o Download/Finish/'+GetVideoName.filename+'.'+partial_element[(len(partial_element)-1)]+' Download/'+GetVideoName.filename+'/'+GetVideoName.filename+'*.'+partial_element[(len(partial_element)-1)]+' >/dev/null &'
+    os.system(cmd_to_execute)
+    return False
 
-#Download partial from sohu TV and combine (SSH download)
-#ssh = paramiko.SSHClient()
-#print ServerIP+','+User+','+PassWord
-#ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-#ssh.connect(ServerIP, username=User, password=PassWord)
-#ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
-def DownloadVideotest(inputURL, IP, Port, ServerIP, User, PassWord):
-  ssh = paramiko.SSHClient()
-  print ServerIP+','+User+','+PassWord
-  ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-  #sleep(initial_wait)
-  for i in range(2):
-    try:
-      ssh.connect(ServerIP, username=User, password=PassWord)
-      break
-    except (BadHostKeyException, AuthenticationException,
-            SSHException, socket.error) as e:
-      print e
-      #sleep(interval)
-  #(http_proxy=http://218.60.56.95:8080 ./youtube-dl -g --get-filename http://tv.sohu.com/20140923/n404564864.shtml)
-  print 'Start of getting URL'
-  partial_element = inputURL.split('tv.sohu.com/')
-  partial_element[1]=partial_element[1].replace('/', '_')
-  cmd_to_execute = '(http_proxy=http://'+IP+':'+Port+' ./youtube-dl -g --get-filename '+inputURL+') > source/'+partial_element[1]+'.txt'
-  os.system(cmd_to_execute)
-  print 'End of getting URL'
-  with open('source/'+partial_element[1]+'.txt') as current_file:
-    partial_array = current_file.readlines()
-  GetVideoName = GetVideoFilename()
-  GetVideoName.GetVideoTitle(inputURL)
-  cmd_to_execute = 'mkdir Downloads/'+GetVideoName.filename
-  ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
-  if len(partial_array) == 0:
-    return True
-  print 'Start download'
-  for i in range(0, (len(partial_array)-1), 1):
-    if i%2 == 1:
-      partial_element = partial_array[i].split('-')
-      partial_name = GetVideoName.filename+partial_element[1]
-      partial_name = partial_name.replace('\n', '')
-      cmd_to_execute = 'wget -O Downloads/'+GetVideoName.filename+'/'+partial_name+' '+partial_url+' --proxy=on -c -e "http_proxy=http://'+IP+':'+Port+'"'
-      #cmd_to_execute = 'curl -O Downloads/'+GetVideoName.filename+'/'+partial_name+' '+partial_url+' -c -e "http_proxy=http://'+IP+':'+Port+'"'
-      print cmd_to_execute
-      ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
-      print ssh_stdout.read()
-      print ssh_stderr.read()
-    else:
-      partial_url = partial_array[i].replace('\n', '')
-  print 'End download'
-  #Combine video
-  #require: sudo apt-get install mencoder
-  #mencoder -ovc copy -oac mp3lame -idx -o out.mp4 *.mp4
-  cmd_to_execute = 'mkdir Downloads/Finish'
-  ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
-  partial_element = partial_name.split('.')
-  cmd_to_execute = 'mencoder -ovc copy -oac mp3lame -idx -o Downloads/Finish/'+GetVideoName.filename+'.'+partial_element[(len(partial_element)-1)]+' Downloads/'+GetVideoName.filename+'/'+GetVideoName.filename+'*.'+partial_element[(len(partial_element)-1)]
-  ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
-  return False
+  #Download partial from sohu TV and combine (SSH download)
+  #ssh = paramiko.SSHClient()
+  #print ServerIP+','+User+','+PassWord
+  #ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  #ssh.connect(ServerIP, username=User, password=PassWord)
+  #ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
+  def DownloadVideotest(self, inputURL, IP, Port):
+    #(http_proxy=http://218.60.56.95:8080 ./youtube-dl -g --get-filename http://tv.sohu.com/20140923/n404564864.shtml)
+    print 'Start of getting URL'
+    partial_element = inputURL.split('tv.sohu.com/')
+    partial_element[1]=partial_element[1].replace('/', '_')
+    cmd_to_execute = '(http_proxy=http://'+IP+':'+Port+' ./youtube-dl -g --get-filename '+inputURL+') > source/'+partial_element[1]+'.txt'
+    os.system(cmd_to_execute)
+    print 'End of getting URL'
+    with open('source/'+partial_element[1]+'.txt') as current_file:
+      partial_array = current_file.readlines()
+    GetVideoName = GetVideoFilename()
+    GetVideoName.GetVideoTitle(inputURL)
+    cmd_to_execute = 'mkdir Downloads/'+GetVideoName.filename
+    ssh_stdin, ssh_stdout, ssh_stderr = self.ssh.exec_command(cmd_to_execute)
+    if len(partial_array) == 0:
+      return True
+    print 'Start download'
+    for i in range(0, (len(partial_array)-1), 1):
+      if i%2 == 1:
+        partial_element = partial_array[i].split('-')
+        partial_name = GetVideoName.filename+partial_element[1]
+        partial_name = partial_name.replace('\n', '')
+        cmd_to_execute = 'wget -O Downloads/'+GetVideoName.filename+'/'+partial_name+' '+partial_url+' --proxy=on -c -e "http_proxy=http://'+IP+':'+Port+'"'
+        #cmd_to_execute = 'curl -O Downloads/'+GetVideoName.filename+'/'+partial_name+' '+partial_url+' -c -e "http_proxy=http://'+IP+':'+Port+'"'
+        print cmd_to_execute
+        ssh_stdin, ssh_stdout, ssh_stderr = self.ssh.exec_command(cmd_to_execute)
+        print ssh_stdout.read()
+        print ssh_stderr.read()
+      else:
+        partial_url = partial_array[i].replace('\n', '')
+    print 'End download'
+    #Combine video
+    #require: sudo apt-get install mencoder
+    #mencoder -ovc copy -oac mp3lame -idx -o out.mp4 *.mp4
+    cmd_to_execute = 'mkdir Downloads/Finish'
+    ssh_stdin, ssh_stdout, ssh_stderr = self.ssh.exec_command(cmd_to_execute)
+    partial_element = partial_name.split('.')
+    cmd_to_execute = 'mencoder -ovc copy -oac mp3lame -idx -o Downloads/Finish/'+GetVideoName.filename+'.'+partial_element[(len(partial_element)-1)]+' Downloads/'+GetVideoName.filename+'/'+GetVideoName.filename+'*.'+partial_element[(len(partial_element)-1)]
+    #print cmd_to_execute
+    ssh_stdin, ssh_stdout, ssh_stderr = self.ssh.exec_command(cmd_to_execute)
+    time.sleep(30)
+    cmd_to_execute = 'rm Downloads/'+GetVideoName.filename+' -rf'
+    ssh_stdin, ssh_stdout, ssh_stderr = self.ssh.exec_command(cmd_to_execute)
+    cmd_to_execute = 'source/'+partial_element[1]+'.txt'
+    ssh_stdin, ssh_stdout, ssh_stderr = self.ssh.exec_command(cmd_to_execute)
+    time.sleep(3)
+    return False
 
 #Main
 reload(sys)
 sys.setdefaultencoding('utf8')
-sohuURL = []
-#sohuURL.append('http://tv.sohu.com/item/MTE5NzE0MA==.html')
-#sohuURL.append('http://tv.sohu.com/20130104/n362393817.shtml')
-#sohuURL.append('http://tv.sohu.com/20150111/n407699522.shtml')
-sohuURL.append('http://tv.sohu.com/20141022/n405361376.shtml')
+with open('source/sohuURL.txt') as current_file:
+      sohuURL = current_file.readlines()
+#sohuURL = []
+#sohuURL.append('http://tv.sohu.com/20141022/n405361376.shtml')
+#sohuURL.append('http://tv.sohu.com/20140620/n401112867.shtml')
 ServerList = []
 UserNameList = []
 PassWordList = []
@@ -342,15 +366,17 @@ UserNameList.append('alex')
 PassWordList.append('vuot9442')
 DownloadLimit = 30
 proxyOK = False
-for j in range(0, (len(sohuURL)), 1):
-  ProxyServer = GetAvailableProxyServer()
+ssh=DownloadVideoViaSSH(ServerList[0], UserNameList[0], PassWordList[0])
+ProxyServer = GetAvailableProxyServer()
+for j in range(0, (len(sohuURL)-1), 1):
+  sohuURL[j] = sohuURL[j].replace('\n', '')
   retry = True
   while retry:
     if proxyOK == False:
       ProxyServer.GetProxyIPandPort(sohuURL[j])
     print ProxyServer.currentProxy+':'+ProxyServer.currentPort
     if ProxyServer.currentProxy != '':
-      retry = DownloadVideotest(sohuURL[j], ProxyServer.currentProxy, ProxyServer.currentPort, ServerList[0], UserNameList[0], PassWordList[0])
+      retry = ssh.DownloadVideotest(sohuURL[j], ProxyServer.currentProxy, ProxyServer.currentPort)
       if retry == False:
         proxyOK = True
       else:
